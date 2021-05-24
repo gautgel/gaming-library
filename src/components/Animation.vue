3027578654f54df6676fecf0a535b12332c25b59
gautgel <gelle.gauthier@gmail.com>
2021-05-24 19:30:27 +0200

    start of the project -> animation when loading done

---
+++ src/components/Animation.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="animation">
+        <div class="words" style="textAlign: center">
+            <span id="word">Welcome </span><span id="word">in </span><br><span id="word">Waldhafyr's </span><span id="word">Library!</span>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name:'Animation',
+    data() {
+        return {
+            index: -1,
+        }
+    },
+    methods:{
+      animation: function (params) {
+        const words = document.querySelectorAll("#word"),
+        animation = document.querySelector(".animation");
+        console.log(words);
+        var setInt = setInterval(() => {
+          this.index++;
+          console.log(this.index);
+          const word = words[this.index];
+          console.log(word);
+          word.style.transform = 'scale(1)';
+          word.style.opacity = 1;
+          if (this.index == words.length - 1) {
+            clearInterval(setInt);
+            setTimeout(() => {
+              animation.style.opacity = 0;
+              animation.ontransitionend = ()=>{
+                this.$emit('endAnimation', true);
+              } 
+            },1000);
+          }
+        }, 500);
+      }
+    },
+    mounted() {
+        this.animation();
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Library.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="library">
+        <Animation v-if="!done" v-on:endAnimation="animationEnded($event)"></Animation>
+        <!--<Animation></Animation>-->
+    </div>
+</template>
+<script>
+// @ equals src path
+import Animation from '@/components/Animation.vue'
+export default {
+    name:'Library',
+    components:{
+        Animation
+    },
+    data() {
+        return {
+            done: false,
+            disapear: false,
+        }
+    },
+    methods: {
+        animationEnded: function(event){
+            this.done = event;
+        }
+    },
+}
+</script>
+<style scoped lang="scss">
+</style>
